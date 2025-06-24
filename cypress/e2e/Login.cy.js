9173af0de46b8667182ec6cd8c694e5a93c5e045
anisya <anisya.weeteling@gmail.com>
2025-06-24 15:48:24 +0200

    Login-wip

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,10 +1,23 @@
 import LoginPage from "../pages/LoginPage";
+import MenuPage from "../pages/MenuPage";
 
 describe('Login Test', () => {
-    it('should log in successfully', () => {
+    beforeEach(() => {
         LoginPage.visit();
-        LoginPage.enterStandardUsername("standard_user");
+      });
+
+    it('should log in successfully with valid credentials', () => {
+        LoginPage.enterUsername("standard_user");
         LoginPage.enterPassword("secret_sauce");
         LoginPage.clickLogin();
+        LoginPage.validateProductPage();
+        MenuPage.loggingOut();
     });
+
+    it('should show error validation for invalid user', () => {
+        LoginPage.enterUsername("locked_out_user");
+        LoginPage.enterPassword("secret_sauce");
+        LoginPage.clickLogin();
+        LoginPage.validateErrorLoginMessage();
+      });
 });
