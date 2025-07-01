a70455ba0f5f528eebb822023e16cce8c3683bba
anisya <anisya.weeteling@gmail.com>
2025-07-01 15:44:48 +0200

    add mochawsome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/html',
+      overwrite: false,
+      html: false, // we'll generate HTML after merging JSON
+      json: true
+    },
+
   e2e: {
     watchForFileChanges: false,
     video: true,                 // explicitly enable video recording
     defaultCommandTimeout: 5000, // optional increased timeout
     trashAssetsBeforeRuns: true, // clean old videos/screenshots before tests
     testIsolation: false,
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+    watchForFileChanges: false,
+    chromeWebSecurity: false,
+    blockHosts: ["https://events.backtrace.io"],
+
     setupNodeEvents(on, config) {
       on('after:spec', (spec, results) => {
         if (results && results.failures) {
@@ -14,9 +27,5 @@ module.exports = defineConfig({
         }
       });
     },
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
-    watchForFileChanges: false,
-    chromeWebSecurity: false,
-    blockHosts: ["https://events.backtrace.io"],
   },
 });
