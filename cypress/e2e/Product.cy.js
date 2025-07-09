4cef1c1131ce77c972de418fa86ecfdaa15ee1d0
anisya <anisya.weeteling@gmail.com>
2025-07-09 09:13:15 +0200

    add product new test cases

--- cypress/e2e/Product.cy.js
+++ cypress/e2e/Product.cy.js
@@ -18,5 +18,30 @@ describe('Add to cart', () => {
     it('should display 6 products on the product inventory page', () => {
         ProductPage.validateAmountInventory();
     });
+
+    it('should display all products', () => {
+        ProductPage.getInventoryItems().should('have.length.greaterThan', 0);
+      });
+    
+    it('should add a product to cart', () => {
+        ProductPage.addFirstProductToCart();
+        ProductPage.verifyCartCount(1);
+    });
+    
+    it('should sort products by price (low to high)', () => {
+        ProductPage.sortBy('lohi');
+    });
+    
+    it('should sort products by price (high to low)', () => {
+        ProductPage.sortBy('hilo');
+    });
+    
+    it('should sort products by name (A to Z)', () => {
+        ProductPage.sortBy('az');
+    });
+    
+    it('should sort products by name (Z to A)', () => {
+        ProductPage.sortBy('za');
+    });
 });
 
