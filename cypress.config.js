febd1d7070f69902992c4ad4890aa5126e5b782a
anisya <anisya.weeteling@gmail.com>
2025-07-11 09:46:50 +0200

    add esbuild

--- cypress.config.js
+++ cypress.config.js
@@ -1,49 +1,51 @@
-const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-const createCucumberPlugin = require("@badeball/cypress-cucumber-preprocessor");
-
+const { defineConfig } = require('cypress')
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+const {
+    createEsbuildPlugin,
+} = require('@badeball/cypress-cucumber-preprocessor/esbuild')
+const createCucumberPlugin = require('@badeball/cypress-cucumber-preprocessor')
 
 module.exports = defineConfig({
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
-      reportDir: 'cypress/reports/html',
-      charts: true,
-      reportPageTitle: "Cypress Test Report",
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: false,
+        reportDir: 'cypress/reports/html',
+        charts: true,
+        reportPageTitle: 'Cypress Test Report',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: false,
     },
 
-  e2e: {
-    //specPattern: ["**/*.feature", "cypress/e2e/**/*.{cy,spec}.{js,ts}"],
-    specPattern: "cypress/e2e/**/*.feature",
-    baseUrl: "https://www.saucedemo.com",
-    supportFile: "cypress/support/e2e.js",
-    screenshotOnRunFailure: true,
-    video: true,                 
-    defaultCommandTimeout: 5000,
-    trashAssetsBeforeRuns: true, // clean old videos/screenshots before tests
-    testIsolation: false,
-    watchForFileChanges: false,
-    chromeWebSecurity: false,
-    blockHosts: ["https://events.backtrace.io"],
-    
+    e2e: {
+        //specPattern: ["**/*.feature", "cypress/e2e/**/*.{cy,spec}.{js,ts}"],
+        specPattern: 'cypress/e2e/**/*.feature',
+        baseUrl: 'https://www.saucedemo.com',
+        supportFile: 'cypress/support/e2e.js',
+        screenshotOnRunFailure: true,
+        video: true,
+        defaultCommandTimeout: 5000,
+        trashAssetsBeforeRuns: true, // clean old videos/screenshots before tests
+        testIsolation: false,
+        watchForFileChanges: false,
+        chromeWebSecurity: false,
+        blockHosts: ['https://events.backtrace.io'],
 
-    async setupNodeEvents(on, config) {
-      // Cucumber
-      await createCucumberPlugin.addCucumberPreprocessorPlugin(on, config);
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
+        async setupNodeEvents(on, config) {
+            //Mochawesome
+            require('cypress-mochawesome-reporter/plugin')(on)
 
-      //Mochawesome
-      require("cypress-mochawesome-reporter/plugin")(on);
+            // Cucumber
+            await createCucumberPlugin.addCucumberPreprocessorPlugin(on, config)
 
-      return config;
+            // esbuild for .feature files
+            on(
+                'file:preprocessor',
+                createBundler({
+                    plugins: [createEsbuildPlugin(config)],
+                })
+            )
+
+            return config
+        },
     },
-  },
-});
+})
